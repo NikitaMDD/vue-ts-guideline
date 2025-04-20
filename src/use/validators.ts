edb6362797ceaa601e42a42d8497544629eb2d62
NikitaMDD <nikitamarch702@gmail.com>
2025-04-21 01:53:23 +0300

    Update validators.ts, add date validation

--- src/use/validators.ts
+++ src/use/validators.ts
@@ -20,3 +20,27 @@ export const validateCardNumber = (value: string) => {
     const cardPattern = /^\d{16}$/;
     return cardPattern.test(value);
 };
+
+// Валидация даты
+export const validateDate = (value: string) => {
+    const datePattern = /^(0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[0-2])\.(\d{4})$/;
+
+    if (!datePattern.test(value)) {
+        return false;
+    }
+
+    const [day, month, year] = value.split('.').map(Number);
+    const date = new Date(year, month - 1, day); // Месяцы в JavaScript начинаются с 0
+
+    if (date.getFullYear() !== year || date.getMonth() !== month - 1 || date.getDate() !== day) {
+        return false;
+    }
+
+    const today = new Date();
+
+    if (date > today) {
+        return false;
+    }
+
+    return true;
+}
