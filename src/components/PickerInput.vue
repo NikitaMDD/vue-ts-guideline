840bb9b3125e1dc6ef75bdeb9509b49523f05b5a
NikitaMDD <nikitamarch702@gmail.com>
2025-04-25 22:26:10 +0300

    Now PickerInput written on js

--- src/components/PickerInput.vue
+++ src/components/PickerInput.vue
@@ -2,31 +2,40 @@
   <div v-if="type === 'date'">
     <div class="field">
       <label for="date1">Выберите дату</label>
-      <div class="js-calendar-picker_wrap"><input type="text" class="js-calendar-picker" name="date1" id="date1"><div class="icon"></div></div>
+      <div class="js-calendar-picker_wrap">
+        <input type="text" class="js-calendar-picker" name="date1" id="date1">
+        <div class="icon"></div>
+      </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts">
+<script>
+import { defineProps, onMounted } from "vue";
 
-import {defineProps} from "vue";
-
-const props = defineProps<{
-  type: string;
-}>();
-
-document.addEventListener("DOMContentLoaded", function() {
-  document.querySelectorAll(".js-calendar-picker").forEach(input => {
-    // Кастомный календарь через класс js-calendar-picker
-    new Picker(input, {
-      format: 'DD.MM.YYYY',
-      text: {
-        title: 'Выберите дату',
-      },
+export default {
+  setup() {
+    const props = defineProps({
+      type: String,
     });
-  });
-});
 
+    onMounted(() => {
+      document.querySelectorAll(".js-calendar-picker").forEach(input => {
+        // Кастомный календарь через класс js-calendar-picker
+        new Picker(input, {
+          format: 'DD.MM.YYYY',
+          text: {
+            title: 'Выберите дату',
+          },
+        });
+      });
+    });
+
+    return {
+      props,
+    };
+  },
+};
 </script>
 
 <style scoped>
