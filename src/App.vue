b2b9590c5617773201ec4f46366f8c5d0d7a6c54
NikitaMDD <nikitamarch702@gmail.com>
2025-04-25 22:19:16 +0300

    Now use new component Table

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,43 @@
           @update:selectedItem="selectedItem = $event"
           @toggleDropdown="isDropdownOpen = !isDropdownOpen"/>
       <FileUploader :files-name="'файлы'" :files-array="filesArray"/>
+      <Table :modal="'#testModal'" style="margin: 30px 0 0" :header-elements="tryHeaderElements" :open-modal="openModal"/>
+      <PickerInput :type="'date'"/>
+    </div>
+  </div>
+  <div class="modal fade newDesignModal cur_modal" id="testModal" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Изменение данных</h5>
+          <button type="button" class="modal-close" data-dismiss="modal">
+          </button>
+        </div>
+        <div class="modal-body">
+          <form class="service-form" action="" method="post" enctype="multipart/form-data" name="service-form">
+            <input type="text" name="ID" id="ID" value="" style="display: none; opacity: 0;">
+            <div class="field req">
+              <label for="RECORD_DATE">Дата</label>
+              <input type="text" name="RECORD_DATE" id="RECORD_DATE" class="service-form__input" v-model="recordDate">
+              <div class="field__error">Пожалуйста, заполните поле.</div>
+            </div>
+            <div class="field req">
+              <label for="NAME">Имя</label>
+              <input type="text" name="NAME" id="NAME" class="service-form__input" placeholder="" inputmode="numeric" v-model="recordName">
+              <div class="field__error">Пожалуйста, заполните поле.</div>
+            </div>
+            <div class="field req">
+              <label for="VALUE">Значение</label>
+              <input type="text" name="VALUE" id="VALUE" class="service-form__input" placeholder="" inputmode="numeric" v-model="recordValue">
+              <div class="field__error">Пожалуйста, заполните поле.</div>
+            </div>
+          </form>
+          <div class="service-main_button" @click="saveChanges">
+            Сохранить изменения
+          </div>
+          <div class="service-form__delete_link" data-el_id="51" data-toggle="modal" data-dismiss="modal" data-target="#delElement">Удалить запись</div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -24,13 +61,15 @@
 <script setup lang="ts">
 import Input from "./components/Input.vue";
 import DropdownMenu from "./components/DropdownMenu.vue";
+import Table from "./components/Table.vue";
 import useInput from "./use/input";
-import { watch } from "vue"; //, ref
+import { watch, ref } from "vue";
 import { InputType } from "./enums/InputType.ts";
 import { validateRequired, validateEmail, validatePhone } from './use/validators.ts';
 import useDropdown from "./use/dropdown.ts";
 import FileUploader from "./components/FileUploader.vue";
 import useFiles from "./use/files.ts";
+import PickerInput from "./components/PickerInput.vue";
 
 // Для массива файлов
 const {arrayOfFiles: filesArray} = useFiles();
@@ -39,6 +78,13 @@ const {arrayOfFiles: filesArray} = useFiles();
 const { isError, modelInput: modelName, validate: validateNameFn } = useInput(validateRequired, false);
 const { isError: isErrorPhone, modelInput: modelNamePhone, validate: validatePhoneFn } = useInput(validatePhone, true);
 const { isError: isErrorMail, modelInput: modelNameMail, validate: validateEmailFn } = useInput(validateEmail, true);
+
+const tryHeaderElements:string[] = [
+    "Имя",
+    "Дата",
+    "Значение",
+    // "Четвертый заголовок",
+]
 
 // Данные для выпадающего списка
 const { selectedItem, isDropdownOpen } = useDropdown();
@@ -53,6 +99,34 @@ const dropdownItems: string[] = [
 ];
 
 // Функционал
+
+const recordDate = ref('');
+const recordName = ref('');
+const recordValue = ref('');
+
+interface TableData {
+  name: string;
+  data: string;
+  value: string;
+}
+
+function openModal(data:TableData) {
+
+  console.log(data);
+
+  recordDate.value = data.data;
+  recordName.value = data.name;
+  recordValue.value = data.value;
+
+}
+
+function saveChanges() {
+  console.log("Данные сохранены:", {
+    date: recordDate.value,
+    value: recordValue.value,
+    name: recordName.value,
+  });
+}
 
 // Смотрим как заполняется данные использовалось для тестирования
 watch(modelName, (newValue) => {
